2816f70268cb31e2ac44fe1c0faf2e33ad18bf3c
Nail Amber <johngrammered@gmail.com>
2023-11-26 19:43:34 +0300

    init commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "s21_decimal.h"
+
+int main(void){
+
+    return 0;
+}
---
+++ s21_decimal.h
@@ -0,0 +1,9 @@
+#ifndef S21_DECIMAL_H_
+#define S21_DECIMAL_H_
+
+#define MINUS 0x80000000
+#define SC 0x00ff0000
+
+
+
+#endif  // S21_DECIMAL_H_
